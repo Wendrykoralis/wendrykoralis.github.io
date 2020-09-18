f3b90bc468cce1244a68f4d8ea74d45f12a8b4ea
Wendrykoralis <71485191+Wendrykoralis@users.noreply.github.com>
2020-09-18 02:15:33 -0400

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,13 @@
+ 
+var lugar = document.getElementById("Lugar").value;
+var lat = document.getElementById("Latitud").value;
+var log = document.getElementById("Longitud").value;
+var email = document.getElementById("email").value;
+var  tipv= document.getElementById("usr").value;
+var fec = document.getElementById("date").value;
+var comentario = document.getElementById("comment").value;
+
+function enviar(){
+    var prueba = "hello"
+    alert(this.lugar);
+}
